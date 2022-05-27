d8e4c422bd7fa68996c2faafc2f672e5f047e0c8
turbinex-admin <ms@turbinex.io>
2022-05-26 21:12:03 -0500

    Adding google analytics snippets.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
+
 
 function MyApp({ Component, pageProps }) {
+
+     const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      window.gtag('config', '', {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+    
   return <Component {...pageProps} />
 }
 
