f3b6972e850c3272d85e939532d7c8fa304d85a0
turbinex-admin <ms@turbinex.io>
2022-09-01 04:49:27 +0000

    Fixing GA tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     const handleRouteChange = url => {
-      window.gtag('config', 'G-WWDCZBNTQC', {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
         page_path: url,
       })
     }
